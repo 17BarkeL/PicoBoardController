26498871c437bed3a7d3c4d9035adf783e07773b
Luke Barkess <luke.barkess@icloud.com>
2022-10-12 12:40:07 +0100

    Add project files.

---
+++ PicoBoardController/PicoBoard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoBoardController
+{
+    // https://twiki.cern.ch/twiki/pub/Sandbox/DaqSchoolExercise14/Picoboard_protocol.pdf
+   class PicoBoard
+    {
+        public enum Sensor
+        {
+            ResistanceD = 0,
+            ResistanceC = 1,
+            ResistanceB = 2,
+            Button = 3,
+            ResistanceA = 4,
+            Light = 5,
+            Sound = 6,
+            Slider = 7,
+        }
+        
+        private int[] sensorValues = new int[8];
+        private byte[] buffer = new byte[18];
+        SerialPort picoBoardPort = new SerialPort();
+
+        public void Connect(string portName)
+        {
+            picoBoardPort.PortName = portName;
+            picoBoardPort.BaudRate = 38400;
+            picoBoardPort.ReadTimeout = 10;
+
+            picoBoardPort.Open();
+        }
+
+        public void Disconnect()
+        {
+            picoBoardPort.Close();
+        }
+
+        public int ReadSensor(Sensor sensor)
+        {
+            buffer[0] = 1;
+            picoBoardPort.Write(buffer, 0, 1); // Request data packet
+            int bytesRead = picoBoardPort.Read(buffer, 0, 18); // Recieve data packet
+            Console.WriteLine($"Read {bytesRead} bytes");
+            return sensorValues[(int)sensor];
+        }
+    }
+}
---
+++ PicoBoardController/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoBoardController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PicoBoard picoBoard = new PicoBoard();
+            picoBoard.Connect("COM9");
+
+            bool running = true;
+
+            while (running)
+            {
+                int button = picoBoard.ReadSensor(PicoBoard.Sensor.Button);
+                Console.WriteLine($"Button: {button}");
+                if (Console.ReadLine() == "quit")
+                {
+                    running = false;
+                }
+            }
+
+            picoBoard.Disconnect();
+        }
+    }
+}
